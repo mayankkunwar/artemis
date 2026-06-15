clebertsuconic/activemq-artemis

ARTEMIS-5972: Create a Panama / FFM module for native implementations, which is now introducing libaio. In the configuration we are naming it as ASYNCIO_2.
Co-Authored-By: Clebert Suconic <clebertsuconic@apache.org>

artemis-journal/src/main/java/org/apache/activemq/artemis/core/io/aio2/AIO2Helper.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.artemis.core.io.aio2;
+
+import java.io.File;
+import java.lang.invoke.MethodHandles;
+
+import org.apache.activemq.artemis.core.io.IOCriticalErrorListener;
+import org.apache.activemq.artemis.core.io.SequentialFileFactory;
+import org.apache.activemq.artemis.utils.critical.CriticalAnalyzer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * AIO2 helper for JDK < 22.
+ * This version uses stub implementations that throw UnsupportedOperationException.
+ * For JDK 22+, see the real implementation in src/main/java22.
+ */
+public class AIO2Helper {
+
+   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+   public static boolean isSupported() {
+      logger.info("AIO2Helper from earlier JDKs being used");
+      return false;
+   }
+
+   public static long getTotalMaxIO() {
+      return 0;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir, int maxIO) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    IOCriticalErrorListener listener,
+                                                                    int maxIO) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    int bufferSize,
+                                                                    int bufferTimeout,
+                                                                    int maxIO,
+                                                                    boolean logRates) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    int bufferSize,
+                                                                    int bufferTimeout,
+                                                                    int maxIO,
+                                                                    boolean logRates,
+                                                                    IOCriticalErrorListener listener,
+                                                                    CriticalAnalyzer analyzer) {
+      return null;
+   }
+
+}

tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/asyncio2/AIO2TestBase.java
@@ -1,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.tests.unit.core.asyncio2;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.core.io.IOCallback;
+import org.apache.activemq.artemis.core.io.aio2.AIO2Helper;
+import org.apache.artemis.nativo.jlibaio.LibaioContext;
+import org.apache.artemis.nativo.jlibaio.LibaioFile;
+import org.apache.activemq.artemis.tests.util.ActiveMQTestBase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * The base class for AIO Tests
+ */
+public abstract class AIO2TestBase extends ActiveMQTestBase {
+   // The AIO Test must use a local filesystem. Sometimes $HOME is on a NFS on
+   // most enterprise systems
+
+   protected String fileName = "fileUsedOnNativeTests.log";
+
+   @Override
+   @BeforeEach
+   public void setUp() throws Exception {
+      super.setUp();
+      assertTrue(AIO2Helper.isSupported(), String.format("libAIO is not loaded on %s %s %s", System.getProperty("os.name"), System.getProperty("os.arch"), System.getProperty("os.version")));
+   }
+
+   @Override
+   @AfterEach
+   public void tearDown() throws Exception {
+      assertEquals(0, AIO2Helper.getTotalMaxIO());
+
+      super.tearDown();
+   }
+
+   protected void encodeBufer(final ByteBuffer buffer) {
+      buffer.clear();
+      int size = buffer.limit();
+      for (int i = 0; i < size - 1; i++) {
+         buffer.put((byte) ('a' + i % 20));
+      }
+
+      buffer.put((byte) '\n');
+
+   }
+
+   protected void preAlloc(final LibaioFile controller, final long size) throws ActiveMQException, IOException {
+      controller.fill(controller.getBlockSize(), size);
+   }
+
+   protected static class CountDownCallback implements IOCallback {
+
+      private final CountDownLatch latch;
+
+      private final List<Integer> outputList;
+
+      private final int order;
+
+      private final AtomicInteger errors;
+
+      public CountDownCallback(final CountDownLatch latch,
+                               final AtomicInteger errors,
+                               final List<Integer> outputList,
+                               final int order) {
+         this.latch = latch;
+
+         this.outputList = outputList;
+
+         this.order = order;
+
+         this.errors = errors;
+      }
+
+      volatile boolean doneCalled = false;
+
+      int errorCalled = 0;
+
+      final AtomicInteger timesDoneCalled = new AtomicInteger(0);
+
+      @Override
+      public void done() {
+         if (outputList != null) {
+            outputList.add(order);
+         }
+         doneCalled = true;
+         timesDoneCalled.incrementAndGet();
+         if (latch != null) {
+            latch.countDown();
+         }
+      }
+
+      @Override
+      public void onError(final int errorCode, final String errorMessage) {
+         new Exception("Error called:: " + errorCode + " message::" + errorMessage).printStackTrace();
+         errorCalled++;
+         if (outputList != null) {
+            outputList.add(order);
+         }
+         if (errors != null) {
+            errors.incrementAndGet();
+         }
+         if (latch != null) {
+            // even thought an error happened, we need to inform the latch,
+            // or the test won't finish
+            latch.countDown();
+         }
+      }
+
+      public static void checkResults(final int numberOfElements, final ArrayList<Integer> result) {
+         assertEquals(numberOfElements, result.size());
+         int i = 0;
+         for (Integer resultI : result) {
+            assertEquals(i++, resultI.intValue());
+         }
+      }
+   }
+
+}

mayankkunwar/artemis

artemis-journal/src/main/java/org/apache/activemq/artemis/core/io/aio2/AIO2Helper.java
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.artemis.core.io.aio2;
+
+import java.io.File;
+import java.lang.invoke.MethodHandles;
+
+import org.apache.activemq.artemis.core.io.IOCriticalErrorListener;
+import org.apache.activemq.artemis.core.io.SequentialFileFactory;
+import org.apache.activemq.artemis.utils.critical.CriticalAnalyzer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * AIO2 helper for JDK less than version 24.
+ * This version uses stub implementations that throw UnsupportedOperationException.
+ * For JDK 24+, see the real implementation in src/main/java24.
+ */
+public class AIO2Helper {
+
+   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+   public static boolean isSupported() {
+      logger.debug("AIO2Helper from earlier JDKs being used");
+      return false;
+   }
+
+   public static long getTotalMaxIO() {
+      return 0;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir, int maxIO) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    IOCriticalErrorListener listener,
+                                                                    int maxIO) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    int bufferSize,
+                                                                    int bufferTimeout,
+                                                                    int maxIO,
+                                                                    boolean logRates) {
+      return null;
+   }
+
+   public static SequentialFileFactory getAIO2SequentialFileFactory(File journalDir,
+                                                                    int bufferSize,
+                                                                    int bufferTimeout,
+                                                                    int maxIO,
+                                                                    boolean logRates,
+                                                                    IOCriticalErrorListener listener,
+                                                                    CriticalAnalyzer analyzer) {
+      return null;
+   }
+
+}

tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/asyncio2/AIO2TestBase.java
@@ -1,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.tests.unit.core.asyncio2;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.core.io.IOCallback;
+import org.apache.activemq.artemis.core.io.aio2.AIO2Helper;
+import org.apache.artemis.nativo.jlibaio.LibaioFile;
+import org.apache.activemq.artemis.tests.util.ActiveMQTestBase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+/**
+ * The base class for AIO Tests
+ */
+public abstract class AIO2TestBase extends ActiveMQTestBase {
+   // The AIO Test must use a local filesystem. Sometimes $HOME is on a NFS on
+   // most enterprise systems
+
+   protected String fileName = "fileUsedOnNativeTests.log";
+
+   @Override
+   @BeforeEach
+   public void setUp() throws Exception {
+      super.setUp();
+      assumeTrue(AIO2Helper.isSupported(), String.format("libAIO is not loaded on %s %s %s", System.getProperty("os.name"), System.getProperty("os.arch"), System.getProperty("os.version")));
+   }
+
+   @Override
+   @AfterEach
+   public void tearDown() throws Exception {
+      assertEquals(0, AIO2Helper.getTotalMaxIO());
+
+      super.tearDown();
+   }
+
+   protected void encodeBufer(final ByteBuffer buffer) {
+      buffer.clear();
+      int size = buffer.limit();
+      for (int i = 0; i < size - 1; i++) {
+         buffer.put((byte) ('a' + i % 20));
+      }
+
+      buffer.put((byte) '\n');
+
+   }
+
+   protected void preAlloc(final LibaioFile controller, final long size) throws ActiveMQException, IOException {
+      controller.fill(controller.getBlockSize(), size);
+   }
+
+   protected static class CountDownCallback implements IOCallback {
+
+      private final CountDownLatch latch;
+
+      private final List<Integer> outputList;
+
+      private final int order;
+
+      private final AtomicInteger errors;
+
+      public CountDownCallback(final CountDownLatch latch,
+                               final AtomicInteger errors,
+                               final List<Integer> outputList,
+                               final int order) {
+         this.latch = latch;
+
+         this.outputList = outputList;
+
+         this.order = order;
+
+         this.errors = errors;
+      }
+
+      volatile boolean doneCalled = false;
+
+      int errorCalled = 0;
+
+      final AtomicInteger timesDoneCalled = new AtomicInteger(0);
+
+      @Override
+      public void done() {
+         if (outputList != null) {
+            outputList.add(order);
+         }
+         doneCalled = true;
+         timesDoneCalled.incrementAndGet();
+         if (latch != null) {
+            latch.countDown();
+         }
+      }
+
+      @Override
+      public void onError(final int errorCode, final String errorMessage) {
+         new Exception("Error called:: " + errorCode + " message::" + errorMessage).printStackTrace();
+         errorCalled++;
+         if (outputList != null) {
+            outputList.add(order);
+         }
+         if (errors != null) {
+            errors.incrementAndGet();
+         }
+         if (latch != null) {
+            // even thought an error happened, we need to inform the latch,
+            // or the test won't finish
+            latch.countDown();
+         }
+      }
+
+      public static void checkResults(final int numberOfElements, final ArrayList<Integer> result) {
+         assertEquals(numberOfElements, result.size());
+         int i = 0;
+         for (Integer resultI : result) {
+            assertEquals(i++, resultI.intValue());
+         }
+      }
+   }
+
+}